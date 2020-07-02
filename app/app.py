8b708a2c2acce8937e9ea34b9a7752109c89ac65
carmelolg <carmelolagamba@hotmail.it>
2020-07-02 23:49:52 +0200

    minor fix

--- app/app.py
+++ app/app.py
@@ -2,6 +2,10 @@
 from flask import Flask
 from flask_restful import Api
 
+import sys
+sys.path.insert(1, '/controllers')
+
+
 from controllers.System import System
 from controllers.Sensor import Sensor
 
