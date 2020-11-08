b3d509858df592365e96cf66fdd1659864b98d65
carmelolg <carmelolagamba@hotmail.it>
2020-11-08 17:46:42 +0100

    minor fix

--- app/services/SensorService.py
+++ app/services/SensorService.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 from app.services.MongoService import MongoService
 from app.utils.JSONEncoder import JSONEncoder
+from datetime import datetime
 
 jsonEncoder = JSONEncoder()
 db = MongoService.getInstance().getDb()
 
-
 class SensorService(object):
-    def save(self, json):
-        result = db.Detection.insert_one(json)
+    def save(self, data):
+        now = datetime.now()
+        current_date = now.isoformat()
+        data['date'] = current_date
+        result = db.Detection.insert_one(data)
         return result.acknowledged
