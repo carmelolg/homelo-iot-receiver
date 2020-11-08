8ea493be3600ebafc1b1a7d0826e7fb7a3503439
carmelolg <carmelolagamba@hotmail.it>
2020-11-08 17:54:47 +0100

    minor fix

--- app/controllers/System.py
+++ app/controllers/System.py
@@ -1,7 +1,10 @@
 from flask_restplus import Resource
-
+from datetime import datetime
 
 class System(Resource):
 
     def get(self):
         return "pong"
+
+    def time(self):
+        return datetime.now()
